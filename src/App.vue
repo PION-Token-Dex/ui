fcec17216bfe5bd4374c13c29b2cf1d97933c882
Krakin't (ˈkrækɪn ɪt) <69094640+krakintgithub@users.noreply.github.com>
2021-06-01 23:30:39 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+
+
+  <div class="hasmetamask">
+    <div class="popup" id="popup">
+      <div class="popup-inner">
+
+        <div class="pion">
+          <img src="./assets/img/bgrnd.jpg" alt="" width="100%" class="bgrnd">
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'APP',
+  created: function () {
+    this.interval = setInterval(() => this.refreshData(), 3000);
+  },
+  data: function () {
+    return {
+      userAddress: 'Connect to Metamask, then reload.',
+    };
+  },
+  beforeMount() {
+    this.refreshData();
+  },
+  methods: { },
+};
+</script>
+
+<style>
+body {background:none transparent !important;}
+
+</style>
